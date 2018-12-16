489784b95dfbef525552ffd9bdf26ae5580c73a1
Christopher Brown <cjbrown102@gmail.com>
2018-12-16 07:10:04 -0800

    cleaning up a bit

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,26 @@
-#[macro_use]
-extern crate prettytable;
 extern crate reqwest;
 extern crate select;
+extern crate feed_rs;
 
 use select::document::Document;
-use select::predicate::{Class, Name, Predicate};
-use prettytable::Table;
+use select::predicate::Name;
 
 fn main() {
-    hacker_news("https://news.ycombinator.com");
+    hacker_news("http://feeds.nightvalepresents.com/welcometonightvalepodcast");
 }
 
 fn hacker_news(url: &str) {
 
-    let resp = reqwest::get(url).unwrap();
+    let mut resp = reqwest::get(url).unwrap();
     assert!(resp.status().is_success());
-
-    let document = Document::from_read(resp).unwrap();
-
-    let mut table = Table::new();
-
-    for node in document.find(Class("athing")) {
-        let rank = node.find(Class("rank")).next().unwrap();
-        let story = node.find(Class("title").descendant(Name("a")))
-            .next()
-            .unwrap()
-            .text();
-        let url = node.find(Class("title").descendant(Name("a")))
-            .next()
-            .unwrap();
-        let url_txt = url.attr("href").unwrap();
-        // shorten strings to make table aesthetically appealing
-        // otherwise table will look mangled by long URLs
-        let url_trim = url_txt.trim_left_matches('/');
-        let rank_story = format!(" | {} | {}", rank.text(), story);
-        // FdBybl-> is formatting syntax specific to
-        // prettytable
-        table.add_row(row![FdBybl->rank_story]);
-        table.add_row(row![Fy->url_trim]);
-    }
-    table.printstd();
+    
+    let parsed = feed_rs::parser::parse(&mut resp).unwrap();
+    println!("{:?}", parsed );
+    /*
+    Document::from_read(resp)
+        .unwrap()
+        .find(Name("enclosure"))
+        .filter_map(|n| n.attr("url"))
+        .for_each(|x| println!("{}", x));
+        */
 }
