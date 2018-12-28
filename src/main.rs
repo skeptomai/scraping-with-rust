8b94676122f8b139ac92ef763dd12d21018b1802
Christopher Brown <cb@skeptomai.com>
2018-12-28 08:17:48 +0000

    simple one background thread version

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate select;
 extern crate tempfile;
 
 use std::fs::File;
-use std::io::Result;
+use std::io::{Error, ErrorKind, Result};
 use std::thread;
 
 fn main() {
@@ -15,14 +15,14 @@ fn hacker_news(url: &str) {
     let mut resp = reqwest::get(url).unwrap();
     assert!(resp.status().is_success());
     let parsed = feed_rs::parser::parse(&mut resp).unwrap();
-
+    /*
     println!(
         "title: {:#?}, number of entries: {}, description: {:#?}",
         parsed.title.unwrap(),
         parsed.entries.len(),
         parsed.description.unwrap()
     );
-    /* for e in parsed.entries {
+    for e in parsed.entries {
         for enc in e.enclosure {
             println!("{:?}", enc.href);
         }
@@ -42,29 +42,36 @@ fn hacker_news(url: &str) {
     handler.join().unwrap();
 }
 
-fn internal_fetch(mp3s: Vec<String>) -> Result<u64> {
+fn get_file_name(response: &reqwest::Response) -> Result<String> {
     let dir = tempfile::tempdir()?;
-    let mut response = reqwest::get(&mp3s[0]).unwrap();
-    assert!(response.status().is_success());
+    let local_name = response
+        .url()
+        .path_segments()
+        .and_then(|segments| segments.last())
+        .and_then(|name| if name.is_empty() { None } else { Some(name) })
+        .unwrap_or("tmp.mp3");
 
-    let fname = {
-        dir.path().join(
-            response
-                .url()
-                .path_segments()
-                .and_then(|segments| segments.last())
-                .and_then(|name| if name.is_empty() { None } else { Some(name) })
-                .unwrap_or("tmp.mp3"),
-        )
-    };
+    Ok(dir.path().join(local_name).to_str().unwrap().to_string())
+}
 
-    println!("File name: {}", fname.to_str().unwrap());
-    let mut f = File::create(fname)?;
-    let bytes_written: u64 = response.copy_to(&mut f).unwrap();
-    Ok(bytes_written)
+fn internal_fetch(mp3: &str) -> Result<u64> {
+    println!("internal_fetch would fetch {}", mp3);
+
+    match reqwest::get(mp3) {
+        Ok(mut response) => {
+            let fname = get_file_name(&response)?;
+            println!("internal_fetch would write {}", fname);
+            let mut f = File::create(fname)?;
+            let bytes_written: u64 = response.copy_to(&mut f).unwrap();
+            println!("Wrote {:#} bytes", bytes_written);
+            Ok(bytes_written)
+        }
+        Err(e) => return Err(Error::new(ErrorKind::Other, e)),
+    }
 }
 
 fn fetch_mp3(mp3s: Vec<String>) {
-    let u = internal_fetch(mp3s).unwrap();
-    println!("{:#?}", &u);
+    for mp3 in mp3s {
+        let _u = internal_fetch(&mp3).unwrap();
+    }
 }
