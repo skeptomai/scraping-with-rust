5b6513bbf1a43eb524c1d5a628dd1ba3648ec052
Christopher Brown <cb@skeptomai.com>
2018-12-31 08:21:15 -0800

    upfactor tempdir badly

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate feed_rs;
+extern crate num_cpus;
 extern crate reqwest;
 extern crate select;
 extern crate tempfile;
@@ -6,6 +7,11 @@ extern crate tempfile;
 use std::fs::File;
 use std::io::{Error, ErrorKind, Result};
 use std::thread;
+
+#[derive(Debug)]
+struct MP3ToFetch {
+    mp3_url: String,
+}
 
 fn main() {
     hacker_news("http://feeds.nightvalepresents.com/welcometonightvalepodcast");
@@ -29,11 +35,15 @@ fn hacker_news(url: &str) {
 
     } */
 
-    let mp3s: Vec<String> = parsed
+    let mp3s: Vec<MP3ToFetch> = parsed
         .entries
         .into_iter()
-        .map(|e| e.enclosure[0].href.clone())
+        .map(|e| MP3ToFetch {
+            mp3_url: e.enclosure[0].href.clone(),
+        })
         .collect();
+
+    println!("{}", num_cpus::get());
 
     let handler = thread::spawn(move || {
         fetch_mp3(mp3s);
@@ -42,7 +52,7 @@ fn hacker_news(url: &str) {
     handler.join().unwrap();
 }
 
-fn get_file_name(response: &reqwest::Response) -> Result<String> {
+fn get_file_name(response: &reqwest::Response) -> Result<(tempfile::TempDir, String)> {
     let dir = tempfile::tempdir()?;
     let local_name = response
         .url()
@@ -51,7 +61,9 @@ fn get_file_name(response: &reqwest::Response) -> Result<String> {
         .and_then(|name| if name.is_empty() { None } else { Some(name) })
         .unwrap_or("tmp.mp3");
 
-    Ok(dir.path().join(local_name).to_str().unwrap().to_string())
+    let fname = dir.path().join(local_name).to_str().unwrap().to_string();
+    println!("fname is {:#}", fname);
+    Ok((dir, fname))
 }
 
 fn internal_fetch(mp3: &str) -> Result<u64> {
@@ -59,19 +71,41 @@ fn internal_fetch(mp3: &str) -> Result<u64> {
 
     match reqwest::get(mp3) {
         Ok(mut response) => {
-            let fname = get_file_name(&response)?;
+            let (_tempdir, fname) = get_file_name(&response)?;
             println!("internal_fetch would write {}", fname);
-            let mut f = File::create(fname)?;
-            let bytes_written: u64 = response.copy_to(&mut f).unwrap();
+            let mut f = match File::create(fname) {
+                Ok(f) => f,
+                Err(e) => {
+                    println!("File::create failed {:#?}", e);
+                    return Err(Error::new(ErrorKind::Other, e));
+                }
+            };
+            println!("f is {:#?}", f);
+            let bytes_written: u64 = match response.copy_to(&mut f) {
+                Ok(f) => f,
+                Err(e) => {
+                    println!("copy_to failed {:#?}", e);
+                    return Err(Error::new(ErrorKind::Other, e));
+                }
+            };
             println!("Wrote {:#} bytes", bytes_written);
             Ok(bytes_written)
         }
-        Err(e) => return Err(Error::new(ErrorKind::Other, e)),
+        Err(e) => {
+            println!("internal_fetch failed: {:#?}", e);
+            return Err(Error::new(ErrorKind::Other, e));
+        }
     }
 }
 
-fn fetch_mp3(mp3s: Vec<String>) {
+fn fetch_mp3(mp3s: Vec<MP3ToFetch>) {
     for mp3 in mp3s {
-        let _u = internal_fetch(&mp3).unwrap();
+        match internal_fetch(&mp3.mp3_url) {
+            Ok(bytes_written) => println!("Wrote: {}", bytes_written),
+            Err(e) => {
+                println!("Cypress Hill, ya'll fucked up! : {:#?}", e);
+                std::process::exit(-1)
+            }
+        }
     }
 }
