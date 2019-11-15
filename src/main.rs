18b40b7433ed4c50db0c51628fec9aa354ff718a
Christopher Brown <cb@skeptomai.com>
2019-11-15 13:27:40 -0800

    lift & rearrange

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
-extern crate feed_rs;
-extern crate num_cpus;
-extern crate reqwest;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 extern crate scoped_threadpool;
-extern crate select;
 extern crate tempfile;
 
+use log::{debug, error, info};
 use scoped_threadpool::Pool;
 use std::fs::File;
 use std::io::{Error, ErrorKind, Result};
-use std::sync::Arc;
-use tempfile::TempDir;
+use std::path::Path;
+use tempfile::Builder;
+
+const WELCOME_TO_NIGHTVALE: &str = "http://feeds.nightvalepresents.com/welcometonightvalepodcast";
+const DIRECTORY_PREFIX: &str = "nightscrape-";
 
 #[derive(Debug, Clone)]
-struct MP3ToFetch {
-    mp3_dir: std::sync::Arc<TempDir>,
+struct MP3ToFetch<'s> {
+    mp3_path: &'s str,
     mp3_url: String,
 }
 
-impl MP3ToFetch {
+impl<'s> MP3ToFetch<'s> {
     pub fn fetch_mp3(&self) {
         match self.internal_fetch() {
-            Ok(bytes_written) => println!("Wrote: {}", bytes_written),
+            Ok(bytes_written) => info!("Wrote: {}", bytes_written),
             Err(e) => {
-                println!("Cypress Hill, ya'll fucked up! : {:#?}", e);
+                error!("Cypress Hill, ya'll fucked up! : {:#?}", e);
                 std::process::exit(-1)
             }
         }
@@ -30,27 +33,27 @@ impl MP3ToFetch {
 
     fn internal_fetch(&self) -> Result<u64> {
         let mp3 = self.mp3_url.clone();
-        println!("internal_fetch would fetch {}", mp3);
+        debug!("internal_fetch would fetch {}", mp3);
 
         match reqwest::get(mp3.as_str()) {
             Ok(mut response) => {
                 let fname = self.get_file_name(&response)?;
-                println!("internal_fetch would write {}", fname);
+                info!("internal_fetch would write {}", fname);
                 let mut f = File::create(fname.as_str())?;
-                println!("f is {:#?}", f);
+                info!("f is {:#?}", f);
 
                 let bytes_written: u64 = match response.copy_to(&mut f) {
                     Ok(f) => f,
                     Err(e) => {
-                        println!("copy_to failed {:#?}", e);
+                        error!("copy_to failed {:#?}", e);
                         return Err(Error::new(ErrorKind::Other, e));
                     }
                 };
-                println!("Wrote {:#} bytes", bytes_written);
+                info!("Wrote {:#} bytes", bytes_written);
                 Ok(bytes_written)
             }
             Err(e) => {
-                println!("internal_fetch failed: {:#?}", e);
+                error!("internal_fetch failed: {:#?}", e);
                 return Err(Error::new(ErrorKind::Other, e));
             }
         }
@@ -64,7 +67,7 @@ impl MP3ToFetch {
             .and_then(|name| if name.is_empty() { None } else { Some(name) });
 
         if let Some(name) = local_name {
-            if let Some(f) = self.mp3_dir.path().join(name).to_str() {
+            if let Some(f) = Path::new(self.mp3_path).join(name).to_str() {
                 return Ok(f.to_string());
             }
         }
@@ -74,49 +77,41 @@ impl MP3ToFetch {
 }
 
 fn main() {
-    hacker_news("http://feeds.nightvalepresents.com/welcometonightvalepodcast");
+    env_logger::init();
+    nightscrape(WELCOME_TO_NIGHTVALE);
 }
 
-fn hacker_news(url: &str) {
-    let mut resp = reqwest::get(url).unwrap();
-    assert!(resp.status().is_success());
-    let parsed = feed_rs::parser::parse(&mut resp).unwrap();
-    let dir = Arc::new(tempfile::TempDir::new().unwrap());
-    /*
-    println!(
-        "title: {:#?}, number of entries: {}, description: {:#?}",
-        parsed.title.unwrap(),
-        parsed.entries.len(),
-        parsed.description.unwrap()
-    );
-    for e in parsed.entries {
-        for enc in e.enclosure {
-            println!("{:?}", enc.href);
-        }
-
-    } */
-
-    let mp3s: Vec<MP3ToFetch> = parsed
-        .entries
-        .into_iter()
-        .map(|e| {
-            let tdir = dir.clone();
-            MP3ToFetch {
-                mp3_dir: tdir,
-                mp3_url: e.enclosure[0].href.clone(),
-            }
-        })
-        .collect();
-
-    let cpus = num_cpus::get();
-
-    let mut pool = Pool::new(cpus as u32);
-
+fn fetch(mp3s: &Vec<MP3ToFetch>) {
+    let part = &mp3s[0..9];
+    let mut pool = Pool::new(num_cpus::get() as u32);
     pool.scoped(|scoped| {
-        for mp3 in mp3s {
+        for mp3 in part {
             scoped.execute(move || {
                 mp3.fetch_mp3();
             })
         }
     })
+}
+
+fn nightscrape(url: &str) {
+    let mut resp = reqwest::get(url).unwrap();
+    assert!(resp.status().is_success());
+    let parsed = feed_rs::parser::parse(&mut resp).unwrap();
+
+    let dir = Builder::new().prefix(DIRECTORY_PREFIX).tempdir().unwrap();
+    // into_path takes ownership and keeps temp directory from being reaped at exit
+    let d = dir.into_path();
+    let pb = d.to_str().unwrap();
+    info!("Path is {:#?}", pb);
+
+    let mp3s: Vec<MP3ToFetch> = parsed
+        .entries
+        .into_iter()
+        .map(|e| MP3ToFetch {
+            mp3_path: pb,
+            mp3_url: e.enclosure[0].href.clone(),
+        })
+        .collect();
+
+    fetch(&mp3s);
 }
